1a42cbc30c42a1fcc16021cf167a378299e02405
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2021-10-22 09:54:34 +0430

    genre work properly

--- routes/routes.js
+++ routes/routes.js
@@ -1,51 +1,53 @@
 
 const express = require('express')
-const routes=express.Router();
+const routes = express.Router();
 const pool = require("../database");
 
 //home router for best movie
-routes.get("/home",(req,res)=>{
+routes.get("/home", (req, res) => {
     pool.query("SELECT * FROM movies WHERE DailyRate > 2 ", (error, result) => {
         if (error) return console.log(error.message);
         console.log(result);
-        
-        res.render("index", {result});
-
+        res.render("index", { result });
     });
-    
 });
+
 //all movies
-routes.get("/movie",(req,res)=>{
-   
+routes.get("/movie", (req, res) => {
     pool.query("SELECT movies.Title,movies.id,movies.Price,movies.DailyRate,movies.NumberInStock,movies.photo,movies.message,movies.YouTube_link, genres.name AS genre_name FROM movies INNER JOIN genres ON movies.genre_id = genres.id;  ", (error, result) => {
         if (error) return console.log(error.message);
-        res.render("movies", {result});
-
+        res.render("movies", { result });
     });
 });
 
 //search bar option
 routes.post("/movie", async (req, res) => {
     const { search } = req.body;
-
-    pool.query("SELECT * FROM movies WHERE Title like ?", ['%'+search+'%'], (error, result) => {
+    pool.query("SELECT movies.Title,movies.id,movies.Price,movies.DailyRate,movies.NumberInStock,movies.photo,movies.message,movies.YouTube_link, genres.name AS genre_name FROM movies INNER JOIN genres ON movies.genre_id = genres.id   WHERE Title like ?", ['%' + search + '%'], (error, result) => {
         if (error) return console.log(error.message);
-        res.render("movies", {result});
-       
-        });
+        res.render("movies", { result });
+    });
 });
 
 //genres
-routes.get("/genres",(req,res)=>{
+routes.get("/genres", (req, res) => {
     res.render("Genres")
 });
+
+//Customers all all
+routes.get("/customers", (req, res) => {
+    pool.query("SELECT custoemrs.name, customers.phone,custoemrs.photo,customers.isGold,movies.name FROM watch INNER JOIN customers WHERE customer_id=customers.id AND  watch INNER JOIN movies WHERE movie_id =movies.id  ", (error, row) => {
+        if (error) return console.log(error.message);
+        res.render("Customer", { row });
+    });
+});
+
+
 //adding a new customer
 routes.post("/customers", async (req, res) => {
-    const { name,phone, isGold, photo } = req.body;
-   
+    const { name, phone, isGold, photo } = req.body;
     pool.query("SELECT * FROM customers WHERE phone=?", [phone], (error, result) => {
         if (error) return console.log(error.message);
-
         else if (result.length > 0) {
             return res.render("Customer", {
                 message: "the customer already exist in database"
@@ -56,11 +58,9 @@ routes.post("/customers", async (req, res) => {
             if (result) {
                 pool.query("SELECT * FROM customers  ", (error, row) => {
                     if (error) return console.log(error.message);
-                    message="addete to database successfully.";
-                    res.render("Customer", {row,message});
-            
+                    message = "addete to database successfully.";
+                    res.render("Customer", { row, message });
                 });
-         
             } else if (error) {
                 return res.render("Customer", {
                     message: error.message
@@ -68,105 +68,86 @@ routes.post("/customers", async (req, res) => {
             }
         });
     });
-    
+
 });
 //sarch one customer
 routes.post("/onecustomer", async (req, res) => {
     const { search } = req.body;
-
-    pool.query("SELECT * FROM customers WHERE name like ?", ['%'+search+'%'], (error, row) => {
+    pool.query("SELECT * FROM customers WHERE name like ?", ['%' + search + '%'], (error, row) => {
         if (error) return console.log(error.message);
-        res.render("Customer", {row});
-       
-        });
-});
-
-//Customers all photo
-routes.get("/customers",(req,res)=>{
-   
-    pool.query("SELECT * FROM customers  ", (error, row) => {
-        if (error) return console.log(error.message);
-        
-        res.render("Customer", {row});
+        res.render("Customer", { row });
 
     });
-  
-   
 });
+
+
 //render the form edite
-routes.get("/edite/:id",(req,res)=>{
-  
-    pool.query("SELECT * FROM customers WHERE id=? ",[req.params.id], (error, row) => {
+routes.get("/edite/:id", (req, res) => {
+    pool.query("SELECT * FROM customers WHERE id=? ", [req.params.id], (error, row) => {
         if (error) return console.log(error.message);
-    
-        res.render("edite",{row});
-        
-
+        res.render("edite", { row });
     });
-   
-    
 });
+
+
 //ubdate a customer
-routes.post("/edite/:id",(req,res)=>{
-    const { name,phone, isGold, photo } = req.body;
-    pool.query("UPDATE customers SET name=? ,phone=? ,isGold=? ,photo=? WHERE id=? ",[name,phone,isGold,photo,req.params.id], (error, row) => {
+routes.post("/edite/:id", (req, res) => {
+    const { name, phone, isGold, photo } = req.body;
+    pool.query("UPDATE customers SET name=? ,phone=? ,isGold=? ,photo=? WHERE id=? ", [name, phone, isGold, photo, req.params.id], (error, row) => {
         if (error) return console.log(error.message);
-        
-        const alert="success fully edited.";
+
+        const alert = "success fully edited.";
         pool.query("SELECT * FROM customers  ", (error, row) => {
             if (error) return console.log(error.message);
-            allert2="Deleted successfully.";
-            res.render("Customer", {row,alert});
-    
+            allert2 = "Deleted successfully.";
+            res.render("Customer", { row, alert });
         });
-        
-
     });
-   
-    
-})
+});
+
 //delete a customer
-routes.get("/customer/:id",(req,res)=>{
-     pool.query("DELETE FROM customers WHERE id =?",[req.params.id],(error, result)=>{
+routes.get("/customer/:id", (req, res) => {
+    pool.query("DELETE FROM customers WHERE id =?", [req.params.id], (error, result) => {
         if (error) return console.log(error.message);
         pool.query("SELECT * FROM customers  ", (error, row) => {
             if (error) return console.log(error.message);
-            allert2="Deleted successfully.";
-            res.render("Customer", {row,allert2});
-    
-        });
-     });
-});
-routes.get("/form/:id",(req,res)=>{
-    pool.query("SELECT id FROM movies WHERE id=? ",[req.params.id], (error, row) => {
-        if (error) return console.log(error.message);
-    
-        res.render("form",{row});
-        
+            allert2 = "Deleted successfully.";
+            res.render("Customer", { row, allert2 });
 
+        });
     });
 });
+
+//render the form 
+routes.get("/form/:id", (req, res) => {
+    pool.query("SELECT id FROM movies WHERE id=? ", [req.params.id], (error, row) => {
+        if (error) return console.log(error.message);
+        res.render("form", { row });
+    });
+});
+
 //buy ticket
-routes.post("/watch/:id",(req,res)=>{
-    pool.query("SELECT id FROM movies WHERE id=? ",[req.params.id], (error, row) => {
+routes.post("/watch/:id", (req, res) => {
+    pool.query("SELECT id FROM movies WHERE id=? ", [req.params.id], (error, row) => {
         if (error) return console.log(error.message);
         console.log(row);
-        pool.query("SELECT id FROM customers WHERE phone=? ",[req.body.phone], (error, result) => {
-            const message=""
-            if (error) return  res.render("form",{row});
-            console.log(result);
-            pool.query("INSERT INTO watch  SET ?",{customer_id:result,movie_id:row}  , function (error, resolve)  {
-                console.log(resolve);
-
+        pool.query("SELECT id FROM customers WHERE phone=? ", [req.body.phone], (error, result) => {
+            const message = "your are not registered in database please sign up";
+            if (result.length == 0) return res.render("form", { row, message });
+            pool.query("INSERT INTO watch  SET ?", { customer_id: result, movie_id: row }, function (error, resolve) {
                 if (resolve) {
-                     res.render("index");
-                }else return console.log(error.message);
+                    res.render("index");
+                } else return console.log(error.message);
             });
-            
-    
         });
-        
-
     });
 });
-module.exports=routes
+//Genre select all
+routes.get("/genre/:id", async (req, res) => {
+    
+    pool.query("SELECT movies.Title,movies.id,movies.Price,movies.DailyRate,movies.NumberInStock,movies.photo,movies.message,movies.YouTube_link, genres.name AS genre_name FROM movies INNER JOIN genres ON movies.genre_id = genres.id   WHERE genre_id = ?",  req.params.id , (error, result) => {
+        if (error) return console.log(error.message);
+        res.render("movies", { result });
+    });
+});
+module.exports = routes
